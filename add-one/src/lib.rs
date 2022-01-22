7149ca3219ba5abb5c33c77b960d4b00a1210c36
Abraham <abrahamalvarez91@gmail.com>
2022-01-22 18:35:30 -0500

    Adding sample dependencies

--- add-one/src/lib.rs
+++ add-one/src/lib.rs
@@ -1,9 +1,10 @@
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+        assert_eq!(3, add_one(2));
     }
 }
 
